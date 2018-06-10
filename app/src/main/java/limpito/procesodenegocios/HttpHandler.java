5e8da975e628f00350436b205d5076a275f243d2
Elisa Boselli <elisaboselli@hotmail.com>
2018-06-10 11:52:54 -0300

    New HttpHandler

--- app/src/main/java/limpito/procesodenegocios/HttpHandler.java
+++ app/src/main/java/limpito/procesodenegocios/HttpHandler.java
@@ -1,23 +1,13 @@
 package limpito.procesodenegocios;
 
 import android.util.Base64;
-import android.util.Log;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
-import java.io.Writer;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import org.json.JSONObject;
 
 
 /**
@@ -26,80 +16,54 @@ import org.json.JSONObject;
 
 public class HttpHandler {
 
-    private static final String TAG = HttpHandler.class.getSimpleName();
+    private final String POST = "POST";
 
     public HttpHandler() {
     }
 
     public String makeServiceCallAuth(String reqUrl, String user, String pass, String method, String body) {
-        String response = null;
+
+        URL url;
+        HttpURLConnection conn;
+        BufferedReader rd;
+        String line;
+        String result = "";
+
         try {
-            URL url = new URL(reqUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            url = new URL(reqUrl);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
             conn.setRequestMethod(method);
+            conn.setRequestProperty("Content-Type", "application/json");
             conn.setRequestProperty("charset", "UTF-8");
 
             String userCredentials = user + ":" + pass;
             String basicAuth = "Basic " + android.util.Base64.encodeToString(userCredentials.getBytes(), Base64.NO_WRAP);
-
             conn.setRequestProperty("Authorization", basicAuth);
 
-            /*if("POST".equals(method)){
-                conn.setDoOutput(true);
-                Writer writer = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream(), "UTF-8"));
-                writer.write(body);
-                writer.close();
-            }*/
-
-            // read the response
-            if("GET".equals(method)) {
-                InputStream in = new BufferedInputStream(conn.getInputStream());
-                response = convertStreamToString(in);
+            if (POST.equals(method)) {
+                //String input = "{ \"action\":\"complete\"} ";
+                //String input = "{\"action\":\"complete\",\"variables\":[{\"name\":\"id_boolvalue\",\"value\":\"false\"}]}";
+                String input = body;
+                OutputStream os = conn.getOutputStream();
+                os.write(input.getBytes());
+                os.flush();
+                os.close();
             }
 
-            if("POST".equals(method)){
-                conn.setDoOutput(true);
-
-                OutputStream out = new BufferedOutputStream(conn.getOutputStream());
-                BufferedWriter writer = new BufferedWriter (new OutputStreamWriter(out, "UTF-8"));
-                writer.write(body);
-                writer.flush();
-                writer.close();
-                out.close();
-
-                conn.connect();
+            rd = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+            while ((line = rd.readLine()) != null) {
+                result += line;
             }
+            rd.close();
 
-        } catch (MalformedURLException e) {
-            Log.e(TAG, "MalformedURLException: " + e.getMessage());
-        } catch (ProtocolException e) {
-            Log.e(TAG, "ProtocolException: " + e.getMessage());
-        } catch (IOException e) {
-            Log.e(TAG, "IOException: " + e.getMessage());
-        } catch (Exception e) {
-            Log.e(TAG, "Exception: " + e.getMessage());
-        }
-        return response;
-    }
+            return result;
 
-    private String convertStreamToString(InputStream is) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-
-        String line;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line).append('\n');
-            }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
-        return sb.toString();
+        return result;
     }
+
 }
