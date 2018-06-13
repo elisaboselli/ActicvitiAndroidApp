4091529e2d2edc8bf3644f10ce7800ec7af1d780
Elisa Boselli <elisaboselli@hotmail.com>
2018-06-13 18:54:55 -0300

    Corrections in list's operations

--- app/src/main/java/limpito/procesodenegocios/Approve_cc.java
+++ app/src/main/java/limpito/procesodenegocios/Approve_cc.java
@@ -3,18 +3,15 @@ package limpito.procesodenegocios;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ListAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import android.content.DialogInterface;
-import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -22,36 +19,32 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Approve_cc extends AppCompatActivity {
 
     private String TAG = Approve_cc.class.getSimpleName();
 
+    private static String ip = "192.168.100.5";
+    private static String url_get = "http://" + ip + ":8080/activiti-rest/service/runtime/tasks?assignee=kermit";
+    private static String url_post = "http://" + ip + ":8080/activiti-rest/service/runtime/tasks/"; // + tasId
+
     private ProgressDialog pDialog;
-    private ListView lv;
 
-    // URL to get tasks
-    //private static String url = "192.168.1.120:8080";
-    private static String url = "192.168.100.3:8080";
-
-    private static String url_get = "http://" + url + "/activiti-rest/service/runtime/tasks?assignee=kermit";
-    private static String url_post = "http://" + url + "/activiti-rest/service/runtime/tasks/"; // + tasId
-
-    ArrayList<HashMap<String, String>> taskList;
+    private ListView listView;
+    private ArrayList<String> taskList;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_approve_cc);
-        //AsyncTaskRunner runner = new AsyncTaskRunner();
-        //runner.execute();
 
         taskList = new ArrayList<>();
-        lv = (ListView) findViewById(R.id.id_task_list);
+        adapter = new ArrayAdapter<>(Approve_cc.this,android.R.layout.simple_list_item_1,taskList);
+        listView = findViewById(R.id.id_task_list);
+
         new GetTasks().execute();
     }
-
 
     private class GetTasks extends AsyncTask<Void, Void, Void> {
 
@@ -67,9 +60,9 @@ public class Approve_cc extends AppCompatActivity {
 
         @Override
         protected Void doInBackground(Void... arg0) {
-            HttpHandler sh = new HttpHandler();
+            HttpHandler handler = new HttpHandler();
 
-            String jsonStr = sh.makeServiceCallAuth(url_get,"kermit","kermit","GET", null);
+            String jsonStr = handler.makeServiceCallAuth(url_get,"kermit","kermit","GET", null);
 
             if (jsonStr != null) {
                 try {
@@ -81,20 +74,10 @@ public class Approve_cc extends AppCompatActivity {
 
                         String id = t.getString("id");
                         String name = t.getString("name");
-                        String description = t.getString("description");
-                        /*String createTime = t.getString("createTime");
-                        String executionId = t.getString("executionId");
-                        String processInstanceId = t.getString("processInstanceId");
-                        String processDefinitionId = t.getString("processDefinitionId");*/
 
-                        HashMap<String, String> task = new HashMap<>();
+                        String taskString = "(" + id + ") "+ name;
+                        taskList.add(taskString);
 
-                        task.put("id", id);
-                        task.put("name", name);
-                        task.put("email", id);
-                        task.put("mobile", description);
-
-                        taskList.add(task);
                     }
                 } catch (final JSONException e) {
                     Log.e(TAG, "Json parsing error: " + e.getMessage());
@@ -131,26 +114,21 @@ public class Approve_cc extends AppCompatActivity {
             super.onPostExecute(result);
             if (pDialog.isShowing())
                 pDialog.dismiss();
-            final ListAdapter adapter = new SimpleAdapter(
-                    Approve_cc.this, taskList,
-                    R.layout.list_item, new String[]{"id", "name",
-                    "otro"}, new int[]{R.id.task_id,
-                    R.id.task_name, R.id.algo_mas});
 
-            lv.setAdapter(adapter);
+            listView.setAdapter(adapter);
 
-            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-                    final String taskId = taskList.get(position).get("id");
+                    final String taskId = taskList.get(position).substring(1,7);
 
                     AlertDialog alertDialog = new AlertDialog.Builder(Approve_cc.this).create();
-                    alertDialog.setTitle(taskList.get(position).get("id"));
+                    alertDialog.setTitle(taskId);
                     alertDialog.setMessage("Info de la task\n Bla bla bla\n");
                     alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Approve",
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
                                     dialog.dismiss();
-                                    PostTaskParams params = new PostTaskParams(taskId,true);
+                                    PostTaskParams params = new PostTaskParams(taskId, position,true);
                                     new PostTask().execute(params);
                                 }
                             });
@@ -158,7 +136,7 @@ public class Approve_cc extends AppCompatActivity {
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
                                     dialog.dismiss();
-                                    PostTaskParams params = new PostTaskParams(taskId,false);
+                                    PostTaskParams params = new PostTaskParams(taskId, position, false);
                                     new PostTask().execute(params);
                                 }
                             });
@@ -185,7 +163,6 @@ public class Approve_cc extends AppCompatActivity {
             pDialog.setMessage("Please wait...");
             pDialog.setCancelable(false);
             pDialog.show();
-
         }
 
         @Override
@@ -197,7 +174,11 @@ public class Approve_cc extends AppCompatActivity {
             HttpHandler handler = new HttpHandler();
             handler.makeServiceCallAuth(url_post + taskId,"kermit","kermit","POST", body);
 
-            taskList.remove(taskId);
+            int size = taskList.size()-1;
+            for(int i=size; i>=0; i--){
+              taskList.remove(i);
+            }
+
             return null;
         }
 
@@ -207,50 +188,25 @@ public class Approve_cc extends AppCompatActivity {
             if (pDialog.isShowing())
                 pDialog.dismiss();
             new GetTasks().execute();
-
         }
 
         private String getBody(boolean approved){
-
-            /* Ejemplo de body
-            {
-                "action" : "complete",
-                "variables" : [
-                    {
-                        "name" : "id_boolvalue",
-                        "value" : "false"
-                    }
-                ]
-            }
-            */
-
-            JSONObject body = new JSONObject();
-            JSONArray variables = new JSONArray();
-            JSONObject status = new JSONObject();
-            try {
-                status.put("name", "id_boolvalue");
-                if (approved)
-                    status.put("value","true");
-                else
-                    status.put("value","false");
-                variables.put(status);
-                body.put("action","complete");
-                body.put("variables",variables);
-                return String.valueOf(body);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return null;
+            if(approved)
+                return "{\"action\":\"complete\",\"variables\":[{\"name\":\"id_boolvalue\",\"value\":\"true\"}]}";
+            else
+                return "{\"action\":\"complete\",\"variables\":[{\"name\":\"id_boolvalue\",\"value\":\"false\"}]}";
         }
 
     }
 
     private static class PostTaskParams {
         String taskId;
+        int position;
         boolean approved;
 
-        PostTaskParams(String taskId, boolean approved) {
+        PostTaskParams(String taskId, int position, boolean approved) {
             this.taskId = taskId;
+            this.position = position;
             this.approved = approved;
         }
     }
