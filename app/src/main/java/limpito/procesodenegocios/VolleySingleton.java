08a549703cefc2b92059fdef62b7168bada7a89e
Elisa Boselli <elisaboselli@hotmail.com>
2018-06-21 15:57:33 -0300

    Use volley

---
+++ app/src/main/java/limpito/procesodenegocios/VolleySingleton.java
@@ -0,0 +1,37 @@
+package limpito.procesodenegocios;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+
+public class VolleySingleton {
+    private static VolleySingleton volleyInstance;
+    private RequestQueue requestQueue;
+    private static Context context;
+
+    private VolleySingleton(Context context){
+        this.context = context;
+        this.requestQueue = getRequestQueue();
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context){
+        if(volleyInstance == null){
+            volleyInstance = new VolleySingleton(context);
+        }
+        return volleyInstance;
+    }
+
+    public RequestQueue getRequestQueue(){
+        if(requestQueue == null){
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public<T> void addToRequestQueue(Request<T> request){
+        getRequestQueue().add(request);
+    }
+}
